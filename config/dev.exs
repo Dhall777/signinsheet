f344282fd0bd2027af5c23225dabe4053a787e7d
Dhall777 <48307756+Dhall777@users.noreply.github.com>
2021-03-21 21:25:55 -0700

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :signinsheet, Signinsheet.Repo,
-  username: "darri",
-  password: "r$pJcntR24Qq*54bAlGsQLQ%3uoOVgJk",
+  username: "postgres",
+  password: "postgres",
   database: "signinsheet_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
